a984823d97aa0bee64d24f15aac31a5daee9280c
Elia R <reutlinger.elia@gmail.com>
2023-01-23 15:27:00 +0100

    Axios issue fix

--- src/plugins/apios/index.js
+++ src/plugins/apios/index.js
@@ -12,11 +12,18 @@ const apios = Axios.create({
 })
 
 apios.interceptors.request.use(config => {
+  /*
   if (pendingCalls[config.baseURL + config.url] === true) {
     return {
-      ...config, cancelToken: new CancelToken((cancel) => cancel(config.url))
+      ...config,
+      cancelToken: new CancelToken(cancel => {
+        console.log('canceling', config.url)
+        cancel(config.url)
+        pendingCalls[config.baseURL + config.url] = null
+      })
     }
   }
+  */
   store.dispatch('app/loading', true)
   pendingCalls[config.baseURL + config.url] = true
   return config
@@ -26,12 +33,12 @@ apios.interceptors.request.use(config => {
 
 apios.interceptors.response.use(res => {
   store.dispatch('app/loading', false)
-  pendingCalls[res.config.url] = null
+  // pendingCalls[res.config.baseURL + res.config.url] = null
   return res.data
 }, err => {
   store.dispatch('app/loading', false)
   if (err.constructor.name === 'Cancel') return Promise.reject(err)
-  pendingCalls[err.response.config.url] = null
+  // pendingCalls[err.response.config.url] = null
   if (!err.response || !err.response.data) return Promise.reject(err)
   if (err.response.data.error_code === 1188) store.dispatch('auth/logout')
   var error = new Error()
